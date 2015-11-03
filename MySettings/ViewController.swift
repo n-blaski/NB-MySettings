ab2f92bb40083d81c4e0426bee402788bf8e99af
webstudent <webstudent@pc16287.local>
2015-11-02 19:19:41 -0600

    Initial Commit

---
+++ MySettings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySettings
+//
+//  Created by webstudent on 11/2/15.
+//  Copyright © 2015 Nic Blaski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
